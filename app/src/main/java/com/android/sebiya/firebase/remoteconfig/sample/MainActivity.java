01ade96f90f9443615736a6f37c30c81064a1fb4
Danny Kim <ssm_sebiya@naver.com>
2018-11-07 23:41:13 +0900

    Initial commit

---
+++ app/src/main/java/com/android/sebiya/firebase/remoteconfig/sample/MainActivity.java
@@ -0,0 +1,115 @@
+package com.android.sebiya.firebase.remoteconfig.sample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import com.android.sebiya.firebase.remoteconfig.RxConfig;
+import com.android.sebiya.firebase.remoteconfig.RxConfig.Config;
+import com.android.sebiya.simplearrayadapter.AbsArrayAdapter.AbsViewBinder;
+import com.android.sebiya.simplearrayadapter.AbsArrayAdapter.OnItemClickListener;
+import com.android.sebiya.simplearrayadapter.SimpleArrayAdapter;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "MainActivity";
+
+    private SimpleArrayAdapter<RxConfig.Config<String>> mAdapter;
+    private FloatingActionButton mFab;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = SimpleArrayAdapter.<RxConfig.Config<String>>with(this)
+                .setLayoutResId(R.layout.list_item)
+                .addViewBinder(R.id.text1, new AbsViewBinder<RxConfig.Config<String>, TextView>() {
+                    @Override
+                    protected void bindView(final TextView textView, final Config<String> stringConfig) {
+                        textView.setText("key : " + stringConfig.key + ", configApplied : " + !stringConfig.isChanged);
+                    }
+                })
+                .withItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(final View view, final int position) {
+                        RxConfig.Config<String> config = mAdapter.getItemByAdapterPosition(position);
+                        Snackbar.make(mFab, config.toString(), Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                })
+                .build();
+
+        recyclerView.setAdapter(mAdapter);
+
+        loadValues();
+
+        mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                List<RxConfig.Config<String>> items = mAdapter.getItems();
+                if (items == null || items.isEmpty()) {
+                    return;
+                }
+
+                for (RxConfig.Config<String> config : items) {
+                    RxConfig.applyConfig(MainActivity.this, config).subscribe();
+                }
+
+                loadValues();
+            }
+        });
+    }
+
+    private void loadValues() {
+        RxConfig.getValues(MainActivity.this)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<Config<String>>>() {
+                    @Override
+                    public void accept(final List<Config<String>> configs) throws Exception {
+                        Log.d(LOG_TAG, "getValues. configs - " + configs);
+                        mAdapter.swapArray(configs);
+                    }
+                });
+    }
+}
---
+++ library/src/main/java/com/android/sebiya/firebase/remoteconfig/RxConfig.java
@@ -0,0 +1,149 @@
+package com.android.sebiya.firebase.remoteconfig;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import io.reactivex.Completable;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.functions.Function;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class RxConfig {
+
+    private static final String LOG_TAG = "RxConfig";
+
+    private static final String PREF_NAME = "remote_config";
+
+    private static final long DEFAULT_CACHE_TIME_SEC = 60 * 60; // 60 min
+
+    public static class Config<T> {
+
+        public final String key;
+
+        public final boolean isChanged;
+
+        public final T remote;
+
+        public final T local;
+
+        public final boolean fromRemote;
+
+        private Config(String key, boolean fromRemote, T local, T remote) {
+            this.key = key;
+            this.local = local;
+            this.remote = remote;
+            this.fromRemote = fromRemote;
+            if (remote != null) {
+                this.isChanged = !remote.equals(local);
+            } else if (local != null) {
+                this.isChanged = !local.equals(remote);
+            } else {
+                this.isChanged = false;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return key + ", changed - " + isChanged + ", fromRemote - " + fromRemote + ", remote - " + remote
+                    + ", local - " + local;
+        }
+    }
+
+    public static Single<Task<Void>> fetch() {
+        return fetch(DEFAULT_CACHE_TIME_SEC);
+    }
+
+    public static Single<Task<Void>> fetch(final long cacheExpiredTime) {
+        return Single.create(new SingleOnSubscribe<Task<Void>>() {
+            @Override
+            public void subscribe(final SingleEmitter<Task<Void>> emitter) throws Exception {
+                getRemoteConfig().fetch(cacheExpiredTime).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            getRemoteConfig().activateFetched();
+                        }
+                        Log.d(LOG_TAG,
+                                "fetch. last status - " + getRemoteConfig().getInfo().getLastFetchStatus()
+                                        + ", fetch time - " + getRemoteConfig().getInfo().getFetchTimeMillis());
+                        emitter.onSuccess(task);
+                    }
+                });
+            }
+        });
+    }
+
+    public static Single<List<Config<String>>> getValues(final Context context) {
+        return fetch().map(new Function<Task<Void>, List<Config<String>>>() {
+            @Override
+            public List<Config<String>> apply(final Task<Void> task) throws Exception {
+                List<Config<String>> configs = new ArrayList<>();
+                Set<String> keySet = getRemoteConfig().getKeysByPrefix("");
+                if (keySet != null) {
+                    for (String key : keySet) {
+                        String remoteValue = getRemoteConfig().getString(key);
+                        String localValue = getLocalConfig(context).getString(key, null);
+                        configs.add(new Config<>(key, task.isSuccessful(), localValue, remoteValue));
+                    }
+                }
+                return configs;
+            }
+        });
+    }
+
+    public static Single<Config<String>> getString(final Context context, final String key) {
+        return fetch().map(new Function<Task<Void>, Config<String>>() {
+            @Override
+            public Config<String> apply(final Task<Void> task) throws Exception {
+                SharedPreferences localConfig = getLocalConfig(context);
+                String localValue = localConfig.getString(key, null);
+                String remoteValue = getRemoteConfig().getString(key);
+                getRemoteConfig().getValue(key);
+                Log.d(LOG_TAG,
+                        "getString. remote - " + remoteValue
+                                + ", local - " + localValue + ", successful - " + task.isSuccessful());
+                return new Config<>(key,
+                        task.isSuccessful(), localValue, remoteValue);
+            }
+        });
+    }
+
+    public static <T> Completable applyConfig(final Context context, final Config<T> config) {
+        return Completable.create(new CompletableOnSubscribe() {
+            @Override
+            public void subscribe(final CompletableEmitter emitter) throws Exception {
+                if (config.isChanged) {
+                    if (config.remote instanceof String) {
+                        getLocalConfig(context).edit().putString(config.key, (String) config.remote).apply();
+                    }
+                    // TODO : handling multiple type
+                }
+                emitter.onComplete();
+            }
+        });
+    }
+
+    private static SharedPreferences getLocalConfig(Context context) {
+        return context
+                .getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    private static FirebaseRemoteConfig getRemoteConfig() {
+        FirebaseRemoteConfig firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        firebaseRemoteConfig.setConfigSettings(
+                new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(false).build());
+        return firebaseRemoteConfig;
+    }
+}
